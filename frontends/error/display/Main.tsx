e383af396ffe53e947a562876c9f977e7853d5fa
Betarost <betarost@yandex.ru>
2024-01-10 10:14:57 +0300

    .

--- frontends/error/display/Main.tsx
+++ frontends/error/display/Main.tsx
@@ -4,7 +4,105 @@ export default function () {
     return (
         <div class="error_wrap">
             <p class="error_text">Страница не найдена</p>
+            <h1 class="error_title"
+            >Error 404</h1>
+            <p class="error_subtitle">Вернитесь на главную страницу</p>
+            <a
+                href="/"
+                onclick={Fn.link}
+                class="btn btn_timing"
+            >
+                <span>На главную</span>
+            </a>
+            <p class="error_text">Страница не найдена</p>
             <h1 class="error_title">Error 404</h1>
+            <p class="error_subtitle">Вернитесь на главную страницу</p>
+            <a
+                href="/"
+                onclick={Fn.link}
+                class="btn btn_timing"
+            >
+                <span>На главную</span>
+            </a>
+            <p class="error_text">Страница не найдена</p>
+            <h1 class="error_title"
+                init={(el) => {
+                    console.log('init первая', el)
+                    const observer = new IntersectionObserver((entries) => {
+                        entries.forEach(entry => {
+                            console.log('=287f08 первая=', entry.isIntersecting, entry)
+
+                        })
+
+                    })
+
+                    observer.observe(el)
+
+                }}
+            >Error 404</h1>
+            <p class="error_subtitle">Вернитесь на главную страницу</p>
+            <a
+                href="/"
+                onclick={Fn.link}
+                class="btn btn_timing"
+            >
+                <span>На главную</span>
+            </a>
+            <p class="error_text">Страница не найдена</p>
+            <h1 class="error_title">Error 404</h1>
+            <p class="error_subtitle">Вернитесь на главную страницу</p>
+            <a
+                href="/"
+                onclick={Fn.link}
+                class="btn btn_timing"
+            >
+                <span>На главную</span>
+            </a>
+            <p class="error_text">Страница не найдена</p>
+            <h1 class="error_title">Error 404</h1>
+            <p class="error_subtitle">Вернитесь на главную страницу</p>
+            <a
+                href="/"
+                onclick={Fn.link}
+                class="btn btn_timing"
+            >
+                <span>На главную</span>
+            </a>
+            <p class="error_text">Страница не найдена</p>
+            <h1 class="error_title">Error 404</h1>
+            <p class="error_subtitle">Вернитесь на главную страницу</p>
+            <a
+                href="/"
+                onclick={Fn.link}
+                class="btn btn_timing"
+            >
+                <span>На главную</span>
+            </a>
+            <p class="error_text">Страница не найдена</p>
+            <h1 class="error_title">Error 404</h1>
+            <p class="error_subtitle">Вернитесь на главную страницу</p>
+            <a
+                href="/"
+                onclick={Fn.link}
+                class="btn btn_timing"
+            >
+                <span>На главную</span>
+            </a>
+            <p class="error_text">Страница не найдена</p>
+            <h1 class="error_title"
+                init={(el) => {
+                    console.log('init последняя', el)
+                    const observer = new IntersectionObserver((entries) => {
+                        entries.forEach(entry => {
+                            console.log('=287f08 последняя=', entry.isIntersecting, entry)
+
+                        })
+
+                    })
+
+                    observer.observe(el)
+                }}
+            >Error 404</h1>
             <p class="error_subtitle">Вернитесь на главную страницу</p>
             <a
                 href="/"
